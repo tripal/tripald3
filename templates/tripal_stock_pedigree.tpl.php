549f4d0f74a495f73db98ddf04a2842713aeb1f2
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2014-12-16 16:33:57 -0600

    small fix to ensure that the description represents the current germplasm

--- templates/tripal_stock_pedigree.tpl.php
+++ templates/tripal_stock_pedigree.tpl.php
@@ -38,7 +38,7 @@
     <div id="block-menu-menu-tree-description" class="block block-menu contextual-links-region">
       <h2>Description</h2>
       <div id="description" class="content">
-      <p>The above tree depicts the parentage of <em>CDC Redberry</em> where each germplasm involved is indicated using a "Germplasm Node" and the relationships between the germplasm are represented as lines connecting Germplasm nodes. Specifically the type of relationship is indicated both using line styles defined in the legend to the right and also in sentence form when you hover your mouse over the relationship lines. Additional information about each germplasm can be obtained by hovering over a Germplasm node. Furthermore, parts of the pedigree diagram can be collapsed or expanded (depending on the state) by clicking on a Germplasm node.</p>
+      <p>The above tree depicts the parentage of <em><?php print $node->stock->name; ?></em> where each germplasm involved is indicated using a "Germplasm Node" and the relationships between the germplasm are represented as lines connecting Germplasm nodes. Specifically the type of relationship is indicated both using line styles defined in the legend to the right and also in sentence form when you hover your mouse over the relationship lines. Additional information about each germplasm can be obtained by hovering over a Germplasm node. Furthermore, parts of the pedigree diagram can be collapsed or expanded (depending on the state) by clicking on a Germplasm node.</p>
       <p>See the legend to the right for a visual reference of the types of relationships shown in the pedigree diagram above.</p>
       </div>
     </div>
