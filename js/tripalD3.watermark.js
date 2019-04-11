aac4858511f7d9fb2e5133445f3ee7741dcf5cec
Reynold Tan <reynoldltan@gmail.com>
2019-04-11 14:09:29 -0600

    watermark visualizations

--- js/tripalD3.watermark.js
+++ js/tripalD3.watermark.js
@@ -3,52 +3,105 @@
  * Add watermark to visualizations.
  */
 
+var stampCount = 0;
+var reqAJAX = false;
+
+// Listen to page for SVGs and stamp watermark each:
+jQuery(document).ready(function() {
+  // Apply watermark:
+  // Set to true to watermark visualizations.
+  var applyWatermark = true;
+
+  if (applyWatermark) {
+    tripalD3DelayWatermark();
+
+    // Listen to select boxes wanting to add another svg-visuals:
+    jQuery('#content select').change(function() {
+      tripalD3DelayWatermark();
+
+      jQuery(document).ajaxComplete(function() {
+        reqAJAX = true;
+        tripalD3DelayWatermark();
+      });
+    });
+
+  }
+});
+
 
 /**
  * Add watermark to visualization.
  */
 function tripalD3Watermark() {
   // Current path, url of the page user is viewing.
-  var l = window.location.href;
+  var l = window.location.href.toLowerCase();
 
   // Apply watermark when page has directory of:
   // Add more directory here where you want a watermark
   // where there's an svg.
+
+  // In lowercase:
   var dir = [
-    'phenotypes',  // All of phenotypes/
-    'LentilTraits' // All of LentilTraits/
+    'phenotypes',   // All of phenotypes/
+    'lentiltraits', // All of LentilTraits/
+    'f1',
+    'cicer',
+    'demo',
     // Add directory here...
   ];
 
-  var overlayId    = 'tripald3-watermark-overlay';
-  // Rule definition in tripald3.css.
-  var overlayClass = 'tripald3-watermark';
-  var overlay      = '<div id="' + overlayId + '">&nbsp;</div>';
 
+  // Each page see if you are looking at any one.
   dir.forEach(function(d) {
-    if(l.indexOf(d) >= 0){
+    if(l.indexOf(d.toLowerCase()) >= 0){
       // Found directory. Find SVG.
       if (jQuery('svg')) {
         // Begin watermark-ing.
-        jQuery('svg').parent().append(overlay);
-        jQuery('#' + overlayId).addClass(overlayClass);
 
-        // Exit here.
+        // Find all svg and stamp.
+        jQuery('svg').each(function(i) {
+          var overlayId    = 'tripald3-watermark-overlay' + i;
+          // Rule definition in tripald3.css.
+          var overlayClass = 'tripald3-watermark';
+          var overlay      = '<div id="' + overlayId + '">&nbsp;</div>';
+
+          jQuery(this).parent().css('position', 'relative').append(overlay);
+          jQuery('#' + overlayId).addClass(overlayClass);
+
+          stampCount++;
+        });
+
+        // Exit here. No need to check other pages.
         return;
       }
     }
   });
 
-  return;
+  return stampCount;
 }
 
 
-// Listen to page and stamp watermark:
-jQuery(document).ready(function() {
-  // Though this is in ready() we want to give
-  // it a wait in case the svg is yet to be present in DOM.
-  var waitLoad = setTimeout(function() {
-    tripalD3Watermark();
+/**
+ * Delay watermark to account for SVG added through
+ * AJAX call or by append().
+ */
+function tripalD3DelayWatermark() {
+  var stamped;
+
+  stamped = tripalD3Watermark();
+
+  if (reqAJAX) {
+    // AJAX takes back watermark, try restamping...
+    stamped = 0;
+    reqAJAX = false;
+  }
+
+  if (stamped > 0) {
+    // When watermark applied, Stop watermark.
     clearTimeout(waitLoad);
-  }, 1000);
-});
+  }
+  else {
+    // Wait, try again, maybe AJAX is adding the svg.
+    var waitLoad = setTimeout(tripalD3DelayWatermark, 500);
+  }
+}
