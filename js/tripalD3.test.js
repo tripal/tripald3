9b58030a14c1c5045d17a956f617d6f3f4a2ce49
marceylewin <60453843+marceylewin@users.noreply.github.com>
2020-04-13 16:19:28 -0400

    Update tripalD3.test.js
    removed check for array, dataLabel and datum in isFrequencyDataCompliant
    added check for numbers in data

--- js/tripalD3.test.js
+++ js/tripalD3.test.js
@@ -122,15 +122,20 @@ tripalD3.test = {
 
     dataLabel = dataLabel || "data";
 
-    if (!Array.isArray(data)) {
+  /**  if (!Array.isArray(data)) {
       console.error("The " + dataLabel + " should be a set where each element has a dataLabel and a datum.");
       return false;
     }
+    */
     if (data.length == 0) {
       console.error("The " + dataLabel + " must not be empty.");
       return false;
     }
     return data.every(function(element) {
+      if (!(typeOf element == 'number')) {
+        return false;
+      }
+  /** return data.every(function(element) {
       if (!("dataLabel" in element)) {
         console.error("Every element of " + dataLabel + " must be an object with a DATALABEL key. This element doesn't comply: " + JSON.stringify(element));
         return false;
@@ -139,6 +144,7 @@ tripalD3.test = {
         console.error("Every element of " + dataLabel + " must be an object with a DATUM key. This element doesn't comply: " + JSON.stringify(element));
         return false;
       }
+      */
       else {
         return true;
       }
