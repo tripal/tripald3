cdd4db4715934a6756847dc031a1c32484a49b18
marceylewin <60453843+marceylewin@users.noreply.github.com>
2020-04-14 21:02:54 -0400

    Update tripalD3.histo.js
    remove drag stipulation
    change ration to 200

--- js/tripalD3.histo.js
+++ js/tripalD3.histo.js
@@ -178,7 +178,7 @@ tripalD3.histo = {
         var y = d3.event.dy;
         var line = d3.select(this);
         var xWidth = max - min;
-        var ration = 500 / xWidth;
+        var ration = 200 / xWidth;
         var offset = xWidth / 2;
         var linePosition = ((lines.attr("x2") / ration) - offset);
         var formatNumber = d3.format(",.0f");
@@ -186,9 +186,9 @@ tripalD3.histo = {
         //var scaleForXAxis = d3.scale.linear().domain([0, width]).range([min, max]);
         var scaledPosition = formatNumber(linePosition);          
  
-    if(x <= min) {
-      dragended() }
-        else {
+   // if(x <= min) {
+   //   dragended() }
+   //   else {
 
         d3.selectAll("rect")
           .attr("fill", function(d) {
@@ -215,7 +215,7 @@ tripalD3.histo = {
         };
 
         line.attr(attributes);
-      }
+     // }
 
       //End drag function
       function dragended() {
