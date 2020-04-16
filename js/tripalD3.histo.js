aa7923ad5b52554cba8f84af74945e2db3c6be22
marceylewin <60453843+marceylewin@users.noreply.github.com>
2020-04-15 20:48:51 -0400

    Update tripalD3.histo.js

--- js/tripalD3.histo.js
+++ js/tripalD3.histo.js
@@ -115,7 +115,7 @@ tripalD3.histo = {
       //Draw the columns
       column.append("rect")
         .attr("x", 1)
-        .attr("y", 0)
+        .attr("y", -50)
         .attr("width", (x(hist[0].dx) - x(0)) - 0.5)
         .attr("height", function(d) {
           return options.height - y(d.y);
