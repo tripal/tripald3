0a963a90c45600a0ed655ef079b3b30df4886aac
marceylewin <60453843+marceylewin@users.noreply.github.com>
2020-04-21 18:17:25 -0400

    Update tripalD3.histo.js

--- js/tripalD3.histo.js
+++ js/tripalD3.histo.js
@@ -195,13 +195,13 @@ tripalD3.histo = {
           d3.selectAll("rect")
             .attr("fill", function(d) {
               if (d.x <= upperLinePosition && d.x >= lowerLinePosition) {return includedColorScale(d.y);} 
-              else if (d.x <= lowerLinePosition && lowerLinePosition < upperLinePosition) {return excludedColorScale(d.y);}
+              else if (d.x < lowerLinePosition && lowerLinePosition < upperLinePosition) {return excludedColorScale(d.y);}
               else if (d.x >= upperLinePosition && d.x >= lowerLinePosition && upperLinePosition < lowerLinePosition) {return includedColorScale(d.y);}
               else if (d.x <= upperLinePosition && lowerLinePosition > upperLinePosition) {return includedColorScale(d.y)}
               else {return excludedColorScale(d.y)}
             })
             .style("stroke", function(d) {
-          	  if (d.x <= upperLinePosition && d.x >= lowerLinePosition) {return highlightColorScale(d.y);} 
+              if (d.x <= upperLinePosition && d.x >= lowerLinePosition) {return highlightColorScale(d.y);} 
               else if (d.x < lowerLinePosition && lowerLinePosition < upperLinePosition) {return includedColorScale(d.y);}
               else if (d.x >= upperLinePosition && d.x >= lowerLinePosition && upperLinePosition < lowerLinePosition) {return highlightColorScale(d.y);}
               else if (d.x <= upperLinePosition && lowerLinePosition > upperLinePosition) {return highlightColorScale(d.y)}
