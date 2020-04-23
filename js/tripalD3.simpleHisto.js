c31caf5f10d167f440153891dc0e1937c2ec6f0f
marceylewin <60453843+marceylewin@users.noreply.github.com>
2020-04-23 18:31:01 -0400

    Update tripalD3.simpleHisto.js
    test to see if it's the code or something else

--- js/tripalD3.simpleHisto.js
+++ js/tripalD3.simpleHisto.js
@@ -2,7 +2,7 @@
  * @file
  * Histogram functionality.
  */
-tripalD3.simpleHisto = {
+tripalD3.histo = {
 
   /**
    * Draw a simple histogram.
@@ -26,8 +26,9 @@ tripalD3.simpleHisto = {
    *     - excludedColor: The base color for the color scale to be applied to the excluded bars.
    *     - highlightColor: The base color for the highlighting to be applied to the included bars.
    */
-    
-  drawSimpleHistogram: function(svg, data, options) {
+  
+  
+  drawHistogram: function(svg, data, options) {
 
     // Check the data is compliant.
     var compliant = tripalD3.test.isFrequencyDataCompliant(data);
@@ -55,10 +56,13 @@ tripalD3.simpleHisto = {
       options.barColor = colors[0];
     }
     
+    //For drag behavior    
+      var drag = d3.behavior.drag();
+          
     //Colors for color scale
       var highlightColor = "#4682B4";
-      var barColor = "#266091";
-    
+      var includedColor = "#266091";
+      var excludedColor = "#ffffff";
     
     //Get max and min of data for X axis
       var max = d3.max(data),
@@ -83,12 +87,17 @@ tripalD3.simpleHisto = {
           .domain([0, yMax])
           .range([options.height - options.yAxisPadding, 0]);
 
-    //Set bar color scale
-      var colorScale = d3.scale.linear()
+    //Set excluded color scale     
+      var excludedColorScale = d3.scale.linear()
          .domain([yMin, yMax])
-         .range([d3.rgb(barColor).brighter(), d3.rgb(barColor).darker()]);
+         .range([d3.rgb(excludedColor).brighter(), d3.rgb(excludedColor).darker()]);
+
+    //Set included color scale
+      var includedColorScale = d3.scale.linear()
+         .domain([yMin, yMax])
+         .range([d3.rgb(includedColor).brighter(), d3.rgb(includedColor).darker()]);
     
-    //Set bar highlight color scale
+    //Set highlight color scale
       var highlightColorScale = d3.scale.linear()
           .domain([yMin, yMax])
           .range([d3.rgb(highlightColor).brighter(), d3.rgb(highlightColor).darker()]);
@@ -107,9 +116,117 @@ tripalD3.simpleHisto = {
           .attr("y", -62)
           .attr("width", (x(hist[0].dx) - x(0)) - 4)
           .attr("height", function(d) {return options.height - y(d.y);})
-          .attr("fill", function(d) {return colorScale(d.y)})
-          .style("stroke", function(d) {return highlightColorScale(d.y)})
+          .attr("fill", function(d) {return excludedColorScale(d.y)})
+          .style("stroke", function(d) {return includedColorScale(d.y)})
           .style("stroke-width", "3px")
+    
+    //Data for the upper threshold line
+      var upperThresholdOrigin = [{
+        'x1': 35,
+        'y1': -62,
+        'x2': 35,
+        'y2': 425
+      }];
+    
+    //Data for the lower threshold line
+      var lowerThresholdOrigin = [{
+        'x1': 30,
+        'y1': -62,
+        'x2': 30,
+        'y2': 425
+      }];  
+
+    //Generate the threshold lines' attributes
+      var lineAttributes = {
+        'x1': function(d) {return d.x1;},
+        'y1': function(d) {return d.y1;},
+        'x2': function(d) {return d.x2;},
+        'y2': function(d) {return d.y2;}
+      };
+
+    //Drag behavior for the threshold lines
+      var drag = d3.behavior.drag()
+          .origin(function(d) {return d;})
+          .on('drag', dragged);
+
+    //Pointer to the threshold lines
+      var line1 = svg.append('line').data(upperThresholdOrigin).attr(lineAttributes).call(drag);
+      var line2 = svg.append('line').data(lowerThresholdOrigin).attr(lineAttributes).call(drag);
+    
+    //Drag function
+      function dragged() {
+          var x = d3.event.dx;
+          var y = d3.event.dy;
+          var line = d3.select(this);
+        
+          //Convert svg coordinates to x-axis coordinates
+          var xAxisScale = d3.scale.linear().domain([0, options.width]).range([min, max]);
+        
+          //Make bar style transitions with threshold movement smoother
+          var upperLinePosition = Math.floor(xAxisScale(line1.attr("x2")));
+          var lowerLinePosition = Math.floor(xAxisScale(line2.attr("x2")));
+        
+          //Format for legend
+          var formatter = d3.format(".2r");
+          var upperScaledPosition = formatter(upperLinePosition); 
+          var lowerScaledPosition = formatter(lowerLinePosition);
+        
+          //Update threshold line properties after drag event
+          var attributes = {
+              x1: parseInt(line.attr('x1')) + x,
+              y1: parseInt(line.attr('y1')),
+
+              x2: parseInt(line.attr('x2')) + x,
+              y2: parseInt(line.attr('y2')),
+          };
+        
+          //For threshold 'container'    
+          var newX1 = attributes.x1;
+    
+          //Revert line to the edge of the chart if dragged too far (out of 'container')
+          attributes.x1 = function(d) {
+              if (newX1 < 30) {
+                return 30;
+              } 
+              else  if (newX1 > 711) {
+                return 711;
+              } 
+              else {
+                return newX1;
+              }
+          };
+          
+          //Make sure lines are straight
+          attributes.x2 = attributes.x1;
+             
+          line.attr(attributes);
+          
+          //Change bar style with threshold movement
+          d3.selectAll(".bars")
+            .attr("fill", function(d) {
+              if (d.x < upperLinePosition && d.x >= lowerLinePosition) {return includedColorScale(d.y);} 
+              else if (d.x < lowerLinePosition && lowerLinePosition < upperLinePosition) {return excludedColorScale(d.y);}
+              else if (d.x >= upperLinePosition && d.x >= lowerLinePosition && upperLinePosition < lowerLinePosition) {return includedColorScale(d.y);}
+              else if (d.x < upperLinePosition && lowerLinePosition > upperLinePosition) {return includedColorScale(d.y)}
+              else {return excludedColorScale(d.y)}
+            })
+            .style("stroke", function(d) {
+              if (d.x < upperLinePosition && d.x >= lowerLinePosition) {return highlightColorScale(d.y);} 
+              else if (d.x < lowerLinePosition && lowerLinePosition < upperLinePosition) {return includedColorScale(d.y);}
+              else if (d.x >= upperLinePosition && d.x >= lowerLinePosition && upperLinePosition < lowerLinePosition) {return highlightColorScale(d.y);}
+              else if (d.x < upperLinePosition && lowerLinePosition > upperLinePosition) {return highlightColorScale(d.y)}
+              else {return includedColorScale(d.y)}
+            })        
+            .style("stroke-width", "3px")                       
+   
+        updateLegend()
+        
+        //Update legend
+        function updateLegend() {
+              d3.selectAll('#legend').html("Max Threshold x: " + upperScaledPosition + "<br/>" + "Min Threshold x: " +  lowerScaledPosition);
+        };   
+        
+    }
     
     //Make x axis
         var xAxis = d3.svg.axis()
@@ -122,6 +239,15 @@ tripalD3.simpleHisto = {
             .attr("class", "x axis")
             .attr("transform", "translate(0," + (options.height - options.yAxisPadding) + ")")
             .call(xAxis);
+    
+    //Add boxes for (very makeshift) color key
+        var colorKeyIncluded = svg.append("rect")
+            .attr({width: 15, height: 15, x: 420, y: 34, fill: includedColor, stroke: highlightColor});
+    
+        var colorKeyExcluded = svg.append("rect")
+            .attr({width: 15, height: 15, x: 420, y: 60, fill: excludedColor, stroke: includedColor});    
+    
+    
   
   },
 };
